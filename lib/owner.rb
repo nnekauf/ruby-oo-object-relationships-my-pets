b9baa7a254cb80828b2458bb00a5dfe894099aad
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-01-29 20:53:53 -0500

    commit tree fbd50f711ef02008ac3811c997bfb4171da1fa4e

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,68 @@
 class Owner
-  # code goes here
+  attr_reader :name
+
+    @@all = []
+    
+  def initialize (name)
+    @name = name
+    species
+    @@all << self
+  end
+
+  def species
+    "human"
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat(cat)
+    Cat.new(cat, self)
+  end
+
+  def buy_dog(dog)
+    Dog.new(dog, self)
+  end
+
+  def walk_dogs
+    Dog.all.each{|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    Cat.all.each{|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    cats.each{|cat| cat.owner = nil}
+    #cats.each{|cat| cat.mood = "nervous"}
+    dogs.each{|dog| dog.owner = nil}
+    #dogs.each{|dog| dog.mood = "nervous"}
+    Cat.all.each{|cat| cat.mood = "nervous"}
+    Dog.all.each{|dog| dog.mood = "nervous"}
+  end
+
+  def list_pets
+    "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+  end
 end
